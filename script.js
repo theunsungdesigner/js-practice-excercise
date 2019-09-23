f9d5ce68bf79dce87152cc7fb7c4a5d61731fe36
theunsungdesigner <theunsungdesigner@gmail.com>
2019-09-23 18:42:35 -0400

    homework changes

---
+++ script.js
@@ -0,0 +1,37 @@
+// // answer to question 1
+  
+//     let pizza = '';
+//     let numberOfPizzas = 10;
+//     let numberOfRows = 10;
+//     for (let i = 0; i < numberOfRows; i++){
+
+    
+//         for (let i = 0; i < numberOfPizzas; i++) {
+//         pizza = pizza + "🐻";
+//         }
+//         pizza += "\n"
+//     }
+//     console.log(pizza);
+
+//   answer to question 2
+
+let triangleEmoji = ""
+let numberOfRows = 8
+
+for (let i = 0; i < numberOfRows; i++) {
+    triangleEmoji += "🔼"
+    console.log(triangleEmoji);
+}
+
+// answer to question 3 
+
+// let triangleEmoji = ""
+// let numberOfRows = 7
+
+// for (let i = 0; i < numberOfRows; i--) {
+//     triangleEmoji -= 8
+//     console.log(triangleEmoji);
+    
+
+// }
+
